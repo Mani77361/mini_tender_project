10f68b4e05abd141c9afc1e0d89269a2536d3980
Mani77361 <mani.hsundhar@gmail.com>
2026-08-31 14:50:03 +0530

    Updated Response

--- src/main/java/com/tender/practice/mini_tenderproject/dto/BidItemResponse.java
+++ src/main/java/com/tender/practice/mini_tenderproject/dto/BidItemResponse.java
@@ -1,5 +1,26 @@
 package com.tender.practice.mini_tenderproject.dto;
 
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BidItemResponse {
 
+	private Long bidItemId;
+	
+	private Long bidId;
+	
+	private Long tenderItemId;
+	
+	private BigDecimal unitPrice;
+	
+	private Integer quantity;
+	
+	private BigDecimal totalPrice;
+	
 }
